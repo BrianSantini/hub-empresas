53388da42f5489276abc3ed2a342b77525e18cfb
lmachado <lmachado@casanova.ar>
2026-02-10 14:06:35 +0000

    Manual code change

--- src/pages/HubPage.jsx
+++ src/pages/HubPage.jsx
@@ -30,6 +30,7 @@ const AVAILABLE_APPS = [
     name: "Informe Leandro",
     url: "https://leandro.base44.app",
     logo:"https://qtrypzzcjebvfcihiynt.supabase.co/storage/v1/object/public/base44-prod/public/698b1ea389075bf898624a83/1c487ea53_image.png"
+    
   },
   {
     id: "estiba",
